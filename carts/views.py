ce15cc7dbb8ec24fff17af4bda11a726099060e4
Brian <cbchien46@gmail.com>
2018-04-20 11:05:02 +0800

    complete basic cart function

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from products.models import Product
 from .models import Cart
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {})
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart": cart_obj})
+
+def cart_update(request):
+    print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print('Check product again.')
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+            print("Removing", product_obj.title)
+        else:
+            cart_obj.products.add(product_obj)
+            print("Adding", product_obj.title)
+        request.session['cart_item_count'] = cart_obj.products.count()
+    return redirect("cart:home")
